c631857a1a7368b8f7d57b345e03a69e3e131601
AdvizeGH <sinister-thoughts@hotmail.com>
2021-04-28 13:22:41 -0500

    Version bump

--- Advize_PlantEverything/PlantEverything.cs
+++ Advize_PlantEverything/PlantEverything.cs
@@ -15,7 +15,7 @@ namespace Advize_PlantEverything
     {
         public const string PluginID = "advize.PlantEverything";
         public const string PluginName = "PlantEverything";
-        public const string Version = "1.4.0";
+        public const string Version = "1.4.1";
 
         private readonly Harmony harmony = new Harmony(PluginID);
 
