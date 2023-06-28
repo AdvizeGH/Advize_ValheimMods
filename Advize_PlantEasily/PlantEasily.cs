3f615e953a31a66e06a475c5235383bc5129fbf7
AdvizeGH <sinister-thoughts@hotmail.com>
2023-06-28 18:56:11 -0500

    Bump PlantEasily version to 1.5.0

--- Advize_PlantEasily/PlantEasily.cs
+++ Advize_PlantEasily/PlantEasily.cs
@@ -14,7 +14,7 @@ namespace Advize_PlantEasily
     {
         public const string PluginID = "advize.PlantEasily";
         public const string PluginName = "PlantEasily";
-        public const string Version = "1.4.4";
+        public const string Version = "1.5.0";
         
         private readonly Harmony Harmony = new(PluginID);
         public static ManualLogSource PELogger = new($" {PluginName}");
