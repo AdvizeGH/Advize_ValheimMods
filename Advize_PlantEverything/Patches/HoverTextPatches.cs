f4797a9b36cf53afa0097e87da76b31b552e14db
Suden <20457922+shudnal@users.noreply.github.com>
2025-04-24 00:07:50 +0300

    Hover text for Plant and Pickable, color gradient, Seasons timer compatibility

--- Advize_PlantEverything/Patches/HoverTextPatches.cs
+++ Advize_PlantEverything/Patches/HoverTextPatches.cs
@@ -1,7 +1,11 @@
 ﻿namespace Advize_PlantEverything;
 
+using BepInEx;
+using BepInEx.Bootstrap;
 using HarmonyLib;
 using System;
+using System.Reflection;
+using UnityEngine;
 using static PlantEverything;
 
 [HarmonyPatch]
@@ -10,26 +14,31 @@ static class HoverTextPatches
     [HarmonyPatch(typeof(Pickable), nameof(Pickable.GetHoverText))]
     static void Postfix(Pickable __instance, ref string __result)
     {
-        if (__instance.m_picked && config.EnablePickableTimers && __instance.m_nview.GetZDO() != null)
+        if (config.EnablePickableTimers && __instance.m_picked && __instance.m_nview && __instance.m_nview.IsValid() && __instance.m_respawnTimeMinutes > 0)
         {
-            if (__instance.m_respawnTimeMinutes == 0) return;
+            long pickedTime = __instance.m_nview.GetZDO().GetLong(ZDOVars.s_pickedTime, 0L);
+            if (pickedTime > 1)
+            {
+                TimeSpan timeSpan = ZNet.instance.GetTime() - new DateTime(pickedTime);
+                double respawnTimeSeconds = GetSecondsToRespawnPickable(__instance);
+                double percent = timeSpan.TotalSeconds / respawnTimeSeconds;
+                
+                string timeString = FormatTime(percent, respawnTimeSeconds - timeSpan.TotalSeconds);
 
-            float growthTime = __instance.m_respawnTimeMinutes * 60;
-            DateTime pickedTime = new(__instance.m_nview.GetZDO().GetLong(ZDOVars.s_pickedTime, 0L));
-            string timeString = FormatTimeString(growthTime, pickedTime);
-
-            __result = Localization.instance.Localize(__instance.GetHoverName()) + $"\n{timeString}";
+                __result = Localization.instance.Localize(__instance.GetHoverName()) + $"\n{timeString}";
+            }
         }
     }
 
     [HarmonyPatch(typeof(Plant), nameof(Plant.GetHoverText))]
     static void Postfix(Plant __instance, ref string __result)
     {
-        if (config.EnablePlantTimers && __instance.m_status == 0 && __instance.m_nview.GetZDO() != null)
+        if (config.EnablePlantTimers && __instance.GetStatus() == Plant.Status.Healthy && __instance.m_nview && __instance.m_nview.IsValid())
         {
-            float growthTime = __instance.GetGrowTime();
-            DateTime plantTime = new(__instance.m_nview.GetZDO().GetLong(ZDOVars.s_plantTime, ZNet.instance.GetTime().Ticks));
-            string timeString = FormatTimeString(growthTime, plantTime);
+            double respawnTimeSeconds = GetSecondsToGrowPlant(__instance);
+            double percent = __instance.TimeSincePlanted() / __instance.GetGrowTime();
+
+            string timeString = FormatTime(percent, respawnTimeSeconds);
 
             __result += $"\n{timeString}";
         }
@@ -62,5 +71,108 @@ static class HoverTextPatches
             $"(<color={color}>Ready any second now</color>)" : $"(Ready in <color={color}>{timeRemaining}</color>)";
 
         return formattedString;
+    }
+
+    private static Gradient gradient;
+
+    public static void UpdateGradient()
+    {
+        if (gradient != null)
+            return;
+
+        gradient = new Gradient();
+        gradient.SetKeys(
+            [
+                new GradientColorKey(Color.red, 0.0f),
+                new GradientColorKey(new Color(1f, 0.5f, 0f), 0.5f), // Orange
+                new GradientColorKey(Color.yellow, 0.75f),
+                new GradientColorKey(new Color(0.2f, 0.8f, 0.2f), 1.0f)  // Limegreen
+            ],
+            Array.Empty<GradientAlphaKey>()
+        );
+    }
+
+    static double GetSecondsToRespawnPickable(Pickable pickable)
+    {
+        if (SeasonsCompatibility.IsReady)
+            return SeasonsCompatibility.GetSecondsToRespawnPickable(pickable);
+
+        return pickable.m_respawnTimeMinutes * 60;
+    }
+
+    static double GetSecondsToGrowPlant(Plant plant)
+    {
+        if (SeasonsCompatibility.IsReady)
+            return SeasonsCompatibility.GetSecondsToGrowPlant(plant);
+
+        return plant.GetGrowTime() - plant.TimeSincePlanted();
+    }
+
+    static string FormatTime(double percent, double secondsToGrow)
+    {
+        UpdateGradient();
+
+        string color = percent < 0 ? "00FFFF" : ColorUtility.ToHtmlStringRGB(gradient.Evaluate(Mathf.Clamp01((float)percent)));
+
+        if (config.GrowthAsPercentage)
+            return $"(<color=#{color}>{percent:P0}</color>)";
+        
+        if (secondsToGrow <= 0)
+            return Localization.instance.Localize("$hud_ready");
+
+        TimeSpan timeSpan = TimeSpan.FromSeconds(secondsToGrow);
+
+        string timeRemaining = timeSpan.Hours > 0
+            ? $"{timeSpan.Hours:D2}h {timeSpan.Minutes:D2}m {timeSpan.Seconds:D2}s"
+            : timeSpan.Minutes > 0
+                ? $"{timeSpan.Minutes:D2}m {timeSpan.Seconds:D2}s"
+                : $"{timeSpan.Seconds:D2}s";
+
+        return secondsToGrow < 60 ? $"(<color=#{color}>Ready any second now</color>)" : $"(Ready in <color=#{color}>{timeRemaining}</color>)";
+    }
+
+    public static class SeasonsCompatibility
+    {
+        private static bool _isInitialized;
+        private static bool _isReady;
+
+        private const string GUID = "shudnal.Seasons";
+        private static Assembly _assembly;
+        private static FieldInfo _seasonState;
+        private static MethodInfo _getSecondsToRespawnPickable;
+        private static MethodInfo _getSecondsToGrowPlant;
+
+        public static bool IsReady
+        {
+            get 
+            {
+                if (!_isInitialized)
+                    Initialize();
+
+                return _isReady;
+            }
+        }
+
+        public static double GetSecondsToRespawnPickable(Pickable pickable) => (double)_getSecondsToRespawnPickable.Invoke(_seasonState.GetValue(_seasonState), [pickable]);
+
+        public static double GetSecondsToGrowPlant(Plant plant) => (double)_getSecondsToGrowPlant.Invoke(_seasonState.GetValue(_seasonState), [plant]);
+
+        private static void Initialize()
+        {
+            _isInitialized = true;
+
+            if (Chainloader.PluginInfos.TryGetValue(GUID, out PluginInfo seasons))
+            {
+                _assembly = Assembly.GetAssembly(seasons.Instance.GetType());
+                if (_assembly != null)
+                {
+                    _seasonState = AccessTools.Field(_assembly.GetType("Seasons.Seasons"), "seasonState");
+                    _getSecondsToRespawnPickable = AccessTools.Method(_seasonState.FieldType, "GetSecondsToRespawnPickable");
+                    _getSecondsToGrowPlant = AccessTools.Method(_seasonState.FieldType, "GetSecondsToGrowPlant");
+                }
+            }
+
+            _isReady = _seasonState != null && _getSecondsToRespawnPickable != null && _getSecondsToGrowPlant != null;
+        }
     }
 }
